7f567b9f680c6e419fd7c4e00a771438366d1add
nakhoadl <nakhoa17@gmail.com>
2017-08-27 15:02:08 +0700

    Acquainted with Json and add some test

--- test/3rd party/jsonTest.cpp
+++ test/3rd party/jsonTest.cpp
@@ -1,16 +1,41 @@
+#include <iostream>
 #include "../../3rd party/json/json.hpp"
 #include "../../3rd party/catch/catch.hpp"
-#include <iostream>
 
 using Json = nlohmann::json;
 
 TEST_CASE("Json") {
-    Json validJson;
-    validJson["Name"] = "Nguyen Anh Khoa";
-    validJson["Age"] = 20;
-    validJson["Hobby"] = {"Code", "Travel", "Girl"};
+    SECTION("Acquainted with Json") {
+        // Create a valid Json and some property
+        // then get string of json to compare
+        Json validJson;
+        validJson["Name"] = "Nguyen Anh Khoa";
+        validJson["Age"] = 20;
+        validJson["Hobby"] = {"Code", "Travel", "Girl"};
 
-    std::string dumpOfJson = validJson.dump();
-    std::string expect;
-    std::cout << dumpOfJson << std::endl;
+        std::string dumpOfJson = validJson.dump();
+        std::string result = "{"
+                             "\"Age\":20,"
+                             "\"Hobby\":[\"Code\",\"Travel\",\"Girl\"],"
+                             "\"Name\":\"Nguyen Anh Khoa\""
+                             "}";
+        CHECK(dumpOfJson == result);
+
+        // Create new Json with static Json::parse(std::string)
+        // then get value to compare
+        Json newJson = Json::parse("{"
+                                   "\"Age\":100,"
+                                   "\"Hobby\":[\"Code\",\"Travel\",\"Girl\"],"
+                                   "\"Name\":\"Nguyen Anh Khoa\""
+                                   "}");
+
+        int resultInt = newJson["Age"];
+        CHECK(resultInt == 100);
+        try {
+            resultInt = newJson["age"];
+        } catch (std::exception &e) {
+            std::cout << e.what() << std::endl;
+        }
+
+    }
 }
